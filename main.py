40f7077c4c5a69a73cd7efbffad8a99394fa4d62
esobirjon2197 <esobirjon2197@gmail.com>
2025-11-12 18:16:43 +0500

    Add various string manipulation examples in main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# 8-m
+matn = "Python o'rganamiz."
+yangi = matn.replace("Python", "Java")
+print(f"{matn} - oldingi")
+print(f"{yangi} - yangi")
+
+
+# 9-m
+i = input("Tel raqm kirit: ")
+print(i)
+yangi = i.replace("-", " ")
+print(yangi)
+
+
+# 10-m
+matn = "Salom dunyo"
+print(matn)
+yangi = matn.replace(" ", "_")
+print(yangi)
+
+
+# 11-m
+i = input("So'z kirit: ")
+if i == "Ko't":
+    print("*****")
+else:
+    print("Yomon")
+
+
+# 12-m
+n = input("Matn kirit: ")
+q = n.find("Python")
+print(q)
+
